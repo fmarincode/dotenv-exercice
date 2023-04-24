118bb24118a1d3764f94affa7bb50d1b2deb040f
Bascom3000 <112181817+Bascom3000@users.noreply.github.com>
2023-04-24 16:00:31 +0200

    exercice done2

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import './App.css';
 
 function App() {
 
-  require('dotenv').config()  
-
+  require('dotenv').config()
+  
   return (
     <div>
       <p>I am ${process.env.YOUR_NAME}, wilder in ${process.env.YOUR_CITY}, and I love ${process.env.YOUR_LANGUAGE}</p>
